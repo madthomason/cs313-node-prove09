16978d47c39232f84774c598045512705c55947b
madthomason <tho16031@byui.edu>
2019-03-09 14:06:50 -0700

    all done now to test on heroku

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/postalCalc', (req, res) => res.render('pages/index'))
+    .get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/usPostalForm.html')))
     .get('/getRate', handlePostalRate);
 
 
@@ -18,26 +17,28 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 function handlePostalRate(req, res) {
     const weight = req.query.weight;
     const type = req.query.type;
+    console.log(weight);
+    console.log(type);
     var result;
     switch (String(type)) {
-        case "letters-stamped"://Letters (Stamped)
+        case "letter-stamped"://Letters (Stamped)
             if (weight > 3) {
                 result = 1.00;
             } else {
                 result = 0.55 + (Math.floor(weight - 1) * .15);
             }
             break;
-        case "letters-metered"://Letters (Metered)
+        case "letter-metered"://Letters (Metered)
             if (weight > 3) {
                 result = 0.95;
             } else {
                 result = 0.50 + (Math.floor(weight - 1) * .15);
             }
             break;
-        case "large-env"://Large Envelopes (Flats)
+        case "large-envelope"://Large Envelopes (Flats)
             result = 1 + (Math.floor(weight - 1) * .15);
             break;
-        case "first-class"://First-Class Package Service—Retail
+        case "first-class-retail-package"://First-Class Package Service—Retail
             if (weight > 12) {
                 result = 5.71;
             } else if (weight >= 8) {
